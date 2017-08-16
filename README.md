1beb408204cf41179ff40de5323e5c4c68100604
Elena Baluyut <ebaluyut@wisc.edu>
2017-08-15 22:04:42 -0700

    adding formulae for gmtsar and gmt

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a repository for hosting homebrew formulae for installing GMT and 
+GMTSAR.  These formulae will be hosted here until they are permanently added to 
+homebrew.  
+
+At the moment, gmtsar.rb is broken.  gmt.rb works.
---
+++ gmtsar.rb
@@ -0,0 +1,47 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Gmtsar < Formula
+  desc "An open source (GNU General Public License) InSAR processing system designed for users familiar with Generic Mapping Tools (GMT). The code is written in C and will compile on any computer where GMT and NETCDF are installed."
+  homepage "http://gmt.soest.hawaii.edu/projects/gmt5sar"
+  url "https://elenacreinisch.com/gmtsar/GMTSAR-5.3.tar.gz"
+  sha256 "321595362d195abaec4a01bebe5e914ef6a0fdc9c7564b8125d3e36151b034c5"
+  # sha256 "ed7c2b8a923787adf47908b888c8fa2e7f81cc496cc56dfca2f88da67aac3de2"
+
+   depends_on "cmake" => :build
+   depends_on "autoconf" => :build
+   depends_on "gmt"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+#    system "./configure", "--disable-debug",
+#                          "--disable-dependency-tracking",
+#                          "--disable-silent-rules",
+#                          "--prefix=#{prefix}"
+#    # system "cmake", ".", *std_cmake_args
+#    system "make", "install" # if this fails, try separate make/make install steps
+#     system "wget", "http://topex.ucsd.edu/gmtsar/tar/ORBITS.tar"
+#     system "mkdir", "$(brew --prefix)/orbits"
+#     system "tar", "xf", "-C", "$(brew --prefix)/orbits"
+     system "autoconf"
+     system "./configure", "--with-orbits-dir=/usr/local/orbits"
+     system "make"
+     system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test GMTSAR`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
